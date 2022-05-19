9e86d3c3b70078e3da12920a39f7ad331b925c7a
bta99 <tuananhvip1099@gmail.com>
2022-05-19 11:47:04 +0700

    T.Anh - push app config demo

---
+++ lib/authorization.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Authorization extends StatefulWidget {
+  const Authorization({Key? key}) : super(key: key);
+
+  @override
+  State<Authorization> createState() => _AuthorizationState();
+}
+
+class _AuthorizationState extends State<Authorization> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
